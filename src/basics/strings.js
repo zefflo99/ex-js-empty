0b1b2c17dea4e82d7e2a892f6a7c4cc2137900d7
kylian <kylianclerc@fedora>
2023-11-20 14:25:14 +0100

    const needleleter

--- src/basics/strings.js
+++ src/basics/strings.js
@@ -1,3 +1,6 @@
+import {match} from "assert";
+
+
 /**
  * Find and replace in the provided string, but preserving case
  * If the new word is longer than the replaced one, ignore tail characters
@@ -7,17 +10,29 @@
  * @return {string} true if n is bigger than 2
  */
 export function findAndReplacePreservingCase(needle, haystack, newWord) {
-  if (typeof needle !== 'String' || typeof haystack !== 'String' || typeof newWord !== 'String') {
+  if (typeof needle !== 'string' || typeof haystack !== 'string' || typeof newWord !== 'string') {
       throw new Error("unsupported types");
   }
 
-
     const regex = new RegExp(needle, 'gi');
+  return haystack.replace(regex, (match) => {
+
+      const needleleter = match.split('')
+      let newWordUp = '';
 
 
-
+  });
 
 }
+
+
+
+
+
+
+
+
+
 
 
 
