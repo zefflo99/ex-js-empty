9a2bd1c7b7ba95dca974adcac20d1ec41bd18e2a
kylian <kylianclerc@fedora>
2023-11-27 16:04:19 +0100

    exo mouvement finish

--- src/events/movements.js
+++ src/events/movements.js
@@ -8,16 +8,15 @@ export function mouseMovements() {
 let screenLog = document.querySelector("#mouse-coordinates");
 document.addEventListener("mousemove", function (event){
 
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
 
-    screenLog.textContent = "x: " + mouseX + ", y: " + mouseY;
-});
-
-
+        screenLog.textContent = "x: " + mouseX + ", y: " + mouseY;
+    });
 }
 
 /**
+ *
  * On the page, you have an input with the id "focus-me".
  * You need to add three behaviors to this input.
  * First, when you hover it, you have to display a message in his label with
@@ -27,7 +26,66 @@ document.addEventListener("mousemove", function (event){
  * Third, when you loose focus of the field, you need to reset the border color to the default one.
  */
 export function hoverFocusAndBlur() {
-  //
+
+    const inputElement = document.querySelector("#focus-me");
+
+    const labels = document.querySelectorAll('label[for="focus-me"]');
+
+    const originalBorderColor = window.getComputedStyle(inputElement).borderColor;
+
+    let previousBorderColor = originalBorderColor;
+
+    let originalLabel = [];
+
+    for (let i of inputElement.labels) {
+        originalLabel.push(i.textContent);
+    }
+
+    function getRandomColor() {
+        const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#00ffff', '#ff00ff'];
+
+        const filteredColors = colors.filter(color => color !== previousBorderColor);
+
+        return filteredColors[Math.floor(Math.random() * filteredColors.length)];
+    }
+
+
+    labels.forEach((label) => {
+
+        inputElement.addEventListener("mouseout", () => {
+
+            for (let i = 0; i < inputElement.labels.length; i++) {
+                inputElement.labels[i].textContent = originalLabel[i];
+            }
+        });
+
+        inputElement.addEventListener("mouseover", function () {
+
+            for (let i of inputElement.labels) {
+                label.textContent = "survole activer";
+            }
+        });
+    });
+
+
+    inputElement.addEventListener("focus", function () {
+
+        const newBorderColor = getRandomColor();
+
+        inputElement.style.borderColor = newBorderColor;
+
+        previousBorderColor = newBorderColor;
+    });
+
+
+    inputElement.addEventListener("blur", function () {
+
+        inputElement.style.borderColor = originalBorderColor;
+    });
+
+
+
+
 }
 
 /**
@@ -39,5 +97,19 @@ export function hoverFocusAndBlur() {
  * Take the opportunity to also apply this colour to the text of the 2 input labels.
  */
 export function changesOnInputEvents() {
-  //
+    const inputElement = document.getElementById('focus-me');
+    const labelElements = document.querySelectorAll('label[for="focus-me"]');
+
+    let defaultBorderColor = '';
+    const getRandomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
+    const updateDefaultColor = () => {
+        defaultBorderColor = getRandomColor();
+        labelElements.forEach(label => label.style.color = defaultBorderColor);
+    };
+    inputElement.addEventListener('input', () => {
+        updateDefaultColor();
+    });
+    inputElement.addEventListener('blur', () => {
+        inputElement.style.borderColor = defaultBorderColor;
+    });
 }
